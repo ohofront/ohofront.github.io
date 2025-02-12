821515326f9ae97a7e27b6e9ef4e8c0bf50b74bc
ohofront <youngho7482@gmail.com>
2025-02-12 12:01:32 +0900

    fix: :bug: deploy 설정 관련 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio-template/',
+  base: '/ohofront.github.io/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
@@ -12,4 +12,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
