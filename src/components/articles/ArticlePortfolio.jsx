9d1ee9c860783a8aae00da115c46633dceab16b9
ohofront <youngho7482@gmail.com>
2025-03-12 17:16:57 +0900

    refactor: :recycle: 포트폴리오 초기 진입 문구 수정

--- src/components/articles/ArticlePortfolio.jsx
+++ src/components/articles/ArticlePortfolio.jsx
@@ -1,115 +1,122 @@
-import React, {useEffect, useState} from 'react'
-import Article from "/src/components/wrappers/Article.jsx"
-import {Col, Row} from "react-bootstrap"
-import {useParser} from "/src/helpers/parser.js"
-import {useScheduler} from "/src/helpers/scheduler.js"
-import Categorizable from "/src/components/capabilities/Categorizable.jsx"
-import Expandable from "/src/components/capabilities/Expandable.jsx"
-import ProjectCard from "/src/components/generic/ProjectCard.jsx"
-import {useLanguage} from "/src/providers/LanguageProvider.jsx"
-import {useWindow} from "/src/providers/WindowProvider.jsx"
+import React, { useEffect, useState } from "react";
+import Article from "/src/components/wrappers/Article.jsx";
+import { Col, Row } from "react-bootstrap";
+import { useParser } from "/src/helpers/parser.js";
+import { useScheduler } from "/src/helpers/scheduler.js";
+import Categorizable from "/src/components/capabilities/Categorizable.jsx";
+import Expandable from "/src/components/capabilities/Expandable.jsx";
+import ProjectCard from "/src/components/generic/ProjectCard.jsx";
+import { useLanguage } from "/src/providers/LanguageProvider.jsx";
+import { useWindow } from "/src/providers/WindowProvider.jsx";
 
 const AnimationStatus = {
-    INVISIBLE: "invisible",
-    VISIBLE: "visible",
-    VISIBLE_WITH_TWEEN: "visible_with_tween"
-}
+  INVISIBLE: "invisible",
+  VISIBLE: "visible",
+  VISIBLE_WITH_TWEEN: "visible_with_tween",
+};
 
 function ArticlePortfolio({ data }) {
-    const parser = useParser()
-    const scheduler = useScheduler()
-    const {isBreakpoint} = useWindow()
-    const {selectedLanguageId} = useLanguage()
+  const parser = useParser();
+  const scheduler = useScheduler();
+  const { isBreakpoint } = useWindow();
+  const { selectedLanguageId } = useLanguage();
 
-    const parsedData = parser.parseArticleData(data)
+  const parsedData = parser.parseArticleData(data);
 
-    const [parsedItems, setParsedItems] = useState([])
-    const [parsedCategories, setParsedCategories] = useState([])
-    const [categoryFilterResult, setCategoryFilterResult] = useState(parsedItems)
-    const [expandableFilterResult, setExpandableFilterResult] = useState(parsedItems)
-    const [shouldAnimate, setShouldAnimate] = useState(false)
-    const [skipAnimationForNextChange, setSkipAnimationForNextChange] = useState(false)
+  const [parsedItems, setParsedItems] = useState([]);
+  const [parsedCategories, setParsedCategories] = useState([]);
+  const [categoryFilterResult, setCategoryFilterResult] = useState(parsedItems);
+  const [expandableFilterResult, setExpandableFilterResult] =
+    useState(parsedItems);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const [skipAnimationForNextChange, setSkipAnimationForNextChange] =
+    useState(false);
 
-    useEffect(() => {
-        const parsedItems = parser.parseArticleItems(parsedData.items)
-        const parsedCategories = parser.parseArticleCategories(parsedData.categories)
-        parser.bindItemsToCategories(parsedItems, parsedCategories)
+  useEffect(() => {
+    const parsedItems = parser.parseArticleItems(parsedData.items);
+    const parsedCategories = parser.parseArticleCategories(
+      parsedData.categories
+    );
+    parser.bindItemsToCategories(parsedItems, parsedCategories);
 
-        setParsedItems(parsedItems)
-        setParsedCategories(parsedCategories)
-        setCategoryFilterResult(parsedItems)
-        setSkipAnimationForNextChange(true)
-    }, [null, selectedLanguageId])
+    setParsedItems(parsedItems);
+    setParsedCategories(parsedCategories);
+    setCategoryFilterResult(parsedItems);
+    setSkipAnimationForNextChange(true);
+  }, [null, selectedLanguageId]);
 
-    useEffect(() => {
-        if(skipAnimationForNextChange) {
-            setSkipAnimationForNextChange(false)
-            return
-        }
-
-        setShouldAnimate(true)
-        _setAnimationStatus(AnimationStatus.INVISIBLE)
-    }, [categoryFilterResult])
-
-    useEffect(() => {
-        if(shouldAnimate) {
-            _setAnimationStatus(AnimationStatus.VISIBLE_WITH_TWEEN)
-        }
-        else {
-            _setAnimationStatus(AnimationStatus.VISIBLE)
-        }
-
-        setShouldAnimate(false)
-    }, [expandableFilterResult])
-
-    useEffect(() => {
-        _setAnimationStatus(AnimationStatus.VISIBLE)
-    }, [])
-
-    const _setAnimationStatus = (animationStatus) => {
-        const tag = 'portfolio-grid'
-        scheduler.clearAllWithTag(tag)
-        const divs = document.querySelectorAll('.grid-item')
-
-        switch(animationStatus) {
-            case AnimationStatus.INVISIBLE:
-                divs.forEach((div, index) => {
-                    div.classList.add(`grid-item-hidden`)
-                })
-                break
-
-            case AnimationStatus.VISIBLE:
-                divs.forEach((div, index) => {
-                    div.classList.remove(`grid-item-hidden`)
-                })
-                break
-
-            case AnimationStatus.VISIBLE_WITH_TWEEN:
-                divs.forEach((div, index) => {
-                    div.classList.add(`grid-item-hidden`)
-
-                    scheduler.schedule(() => {
-                        div.classList.remove(`grid-item-hidden`)
-                    }, 200 + 100 * index, tag)
-                })
-                break
-        }
+  useEffect(() => {
+    if (skipAnimationForNextChange) {
+      setSkipAnimationForNextChange(false);
+      return;
     }
 
-    const _getMaxItemsPerPage = () => {
-        if(isBreakpoint('xxl'))
-            return 9
-        if(isBreakpoint('lg'))
-            return 8
-        if(isBreakpoint('sm'))
-            return 6
-        return 4
+    setShouldAnimate(true);
+    _setAnimationStatus(AnimationStatus.INVISIBLE);
+  }, [categoryFilterResult]);
+
+  useEffect(() => {
+    if (shouldAnimate) {
+      _setAnimationStatus(AnimationStatus.VISIBLE_WITH_TWEEN);
+    } else {
+      _setAnimationStatus(AnimationStatus.VISIBLE);
     }
 
-    return(
-        <Article className={`article-portfolio`} title={ parsedData.title }>
-            <Row className={`gx-4 gy-lg-4 gx-lg-5`}>
-                <Categorizable items={parsedItems}
+    setShouldAnimate(false);
+  }, [expandableFilterResult]);
+
+  useEffect(() => {
+    _setAnimationStatus(AnimationStatus.VISIBLE);
+  }, []);
+
+  const _setAnimationStatus = (animationStatus) => {
+    const tag = "portfolio-grid";
+    scheduler.clearAllWithTag(tag);
+    const divs = document.querySelectorAll(".grid-item");
+
+    switch (animationStatus) {
+      case AnimationStatus.INVISIBLE:
+        divs.forEach((div, index) => {
+          div.classList.add(`grid-item-hidden`);
+        });
+        break;
+
+      case AnimationStatus.VISIBLE:
+        divs.forEach((div, index) => {
+          div.classList.remove(`grid-item-hidden`);
+        });
+        break;
+
+      case AnimationStatus.VISIBLE_WITH_TWEEN:
+        divs.forEach((div, index) => {
+          div.classList.add(`grid-item-hidden`);
+
+          scheduler.schedule(
+            () => {
+              div.classList.remove(`grid-item-hidden`);
+            },
+            200 + 100 * index,
+            tag
+          );
+        });
+        break;
+    }
+  };
+
+  const _getMaxItemsPerPage = () => {
+    if (isBreakpoint("xxl")) return 9;
+    if (isBreakpoint("lg")) return 8;
+    if (isBreakpoint("sm")) return 6;
+    return 4;
+  };
+
+  return (
+    <Article className={`article-portfolio`} title={parsedData.title}>
+      <Row className={`gx-4 gy-lg-4 gx-lg-5`}>
+        <Col className={`col-12 text-center align-center mt-2 p-5`}>
+          업데이트 중입니다..
+        </Col>
+        {/* <Categorizable items={parsedItems}
                                categories={parsedCategories}
                                onFilter={setCategoryFilterResult}
                                storageId={data.id + "_categorizable"}
@@ -136,10 +143,10 @@ function ArticlePortfolio({ data }) {
                             </Col>
                         ))}
                     </Expandable>
-                </Categorizable>
-            </Row>
-        </Article>
-    )
+                </Categorizable> */}
+      </Row>
+    </Article>
+  );
 }
 
-export default ArticlePortfolio
+export default ArticlePortfolio;
